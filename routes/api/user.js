4ce1adc112ebec3c08b1e491e7ae9b69fc6bce94
Bnic3 <bnicemagnolia@gmail.com>
2017-07-09 15:09:20 +0100

    User endpoints refactored to singleton

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,10 +3,10 @@
  */
 var express = require('express');
 var rek = require("rekuire");
-var _ = require("lodash"),
-    moment = require("moment"),
-    Utility= rek("utility")(),
-    jwt = require('jsonwebtoken');
+
+var User = rek("User");
+var UserCtrl = rek("userCtrl")(User);
+
 
 var router = express.Router();
 
@@ -14,40 +14,13 @@ var router = express.Router();
 var DB = rek('database');
 
 router.route('/api/user')
-    .post(createUser);
+    .post(UserCtrl.create);
 
 
 
 
 
-function createUser(req,res){
-    /*eid: Number,
-        phone: String,
-        token: String,
-        r_name: String,
-        status: String,
-        t_exp
-        daily_max: Number,*/
 
-    var input = req.body;
-    //eid, phone
-    var User= DB.model("User");
-    var user = new User();
-    user = _.merge(user, input);
-    user.token= Utility.tokenizer();
-    user.t_exp= moment().add(20,'m').toDate();
-    if (req.body.status){user.status=req.body.status}
-
-    user.save((err,doc)=>{
-        if (err) res.json({error: true, message: "there was an error saving user on the database"});
-        if (!doc) res.json({error: true, message: "Could not save estate on the database"});
-
-
-        return res.json({success:true, message:"User has been created"});
-    });
-
-
-}
 
 
 module.exports = router;
