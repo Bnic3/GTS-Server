6b155ad1997eda9ff0bc055babb1748da0d9248d
Bnic3 <bnicemagnolia@gmail.com>
2017-09-16 17:25:36 +0100

    generate report

--- routes/api/token.js
+++ routes/api/token.js
@@ -12,7 +12,7 @@ var Token = rek("Token");
 var TokenCtrl = rek("tokenCtrl")(Token);
  var middleware = rek("middleware");
 
-
+var Utility= rek("utility")();
 
 var router = express.Router();
 
@@ -22,65 +22,27 @@ var router = express.Router();
 router.post('/api/token',middleware.validateSub,TokenCtrl.create);
 router.route('/api/token/validate').post(TokenCtrl.validate); //eid, code
 router.route('/api/token/old').get(TokenCtrl.old);
-router.route('/api/token/email').get(TokenCtrl.emailstring);
+router.route('/api/token/history').get(TokenCtrl.history);
+router.route('/api/token/reports/:eid/:e_name').get(TokenCtrl.getreports);
 router.route('/api/token/nodemail').get((req, res)=>{
-    var nodemailer = require('nodemailer');
-    var xoauth2 = require('xoauth2');
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            xoauth2: xoauth2.createXOAuth2Generator({
-                user: process.env.MAIL_USER,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                refreshToken: process.env.REFRESH_TOKEN,
-                accessToken: process.env.ACCESS_TOKEN
-            })
-        }
-    })
+    var html = Utility.emalitemp();
 
-    var mailOptions = {
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
         from: process.env.MAIL_USER,
         to: process.env.RECEIVER,
-        subject: 'Nodemailer test',
-        text: 'Hello World!!'
-    }
-
-    transporter.sendMail(mailOptions, function (err, response) {
-        if(err){
-            console.log('Error');
-        } else {
-            console.log('Email Sent');
-            res.send("Email Sent");
-        }
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: html
+    };
+    sgMail.send(msg).then((success)=>{ res.send("Email was sent successfully")
+    }, (failure)=>{
+        console.log("i am in failure");
     });
 
-    // setup email data with unicode symbols
-   /* let mailOptions = {
-        from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
-        to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-        subject: 'Hello ?', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
-    };*/
 
-   /* var transporter = nodemailer.createTransport({
-        sendmail: true,
-        newline: 'unix',
-        path: '/usr/sbin/sendmail'
-    });
-    transporter.sendMail({
-        from: 'sender@example.com',
-        to: 'recipient@example.com',
-        subject: 'Message',
-        text: 'I hope this message gets delivered!',
-        html: '<b>Hello world?</b>' // html body
-    }, (err, info) => {
-        console.log(info.envelope);
-        console.log(info.messageId);
-        res.send("I have sent the email");
-    });*/
 
 });
 
