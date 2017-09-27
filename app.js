42a55e251897f09605c0e6a97bcf426478b3274b
Bnic3 <bnicemagnolia@gmail.com>
2017-09-27 14:23:34 +0100

    sms integration

--- app.js
+++ app.js
@@ -59,7 +59,10 @@ app.use(cors({origin: 'http://localhost:8000'}));
 
 loader(app);
 var rek = require("rekuire");
-rek("housekeeper");
+/*rek("housekeeper");*/
+
+
+
 /*app.use('/', routes);
 app.use('/users', users);*/
 
